ab5de009c6632a1c3086f3f36846f8b50873533e
poramest.vic <poramest.vic@ascendcorp.com>
2022-12-15 09:10:14 +0700

    feat: change ignore package

--- lib/cmd-pkg-ignore-package.bash
+++ lib/cmd-pkg-ignore-package.bash
@@ -3,20 +3,18 @@
 # shellcheck source=./common.bash
 . "$(dirname "${0}")/lib/common.bash"
 
-prepare_file_hook_cmd "$@"
+prepare_repo_hook_cmd "$@"
 
+if [ "${use_dot_dot_dot:-}" -eq 1 ]; then
+	OPTIONS+=('./...')
+fi
 export GO111MODULE=off
-error_code=0
-for sub in $(printf "%q\n" "${FILES[@]}" | xargs -n1 dirname | sort -u); do
-	if [ "${error_on_output:-}" -eq 1 ]; then
-		output=$(/usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}" "./${sub}" 2>&1)
-		if [ -n "${output}" ]; then
-		  echo "linex ${output}"
-			error_code=1
-		fi
-	elif ! /usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}" "./${sub}"; then
-	  echo "liney ${output}"
-		error_code=1
+if [ "${error_on_output:-}" -eq 1 ]; then
+	output=$(/usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}" 2>&1)
+	if [ -n "${output}" ]; then
+		printf "%s\n" "${output}"
+		exit 1
 	fi
-done
-exit $error_code
+else
+	/usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}"
+fi
